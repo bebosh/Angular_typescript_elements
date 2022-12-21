edef1ce2cc86e0607fbc6936a7723e42e140eac0
Paolo Floreano <pieffelleo@gmail.com>
2022-12-21 12:48:39 +0100

    pipes example

---
+++ formPipes/src/app/app.component.ts
@@ -0,0 +1,26 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+})
+export class AppComponent {
+  name: string = '';
+  date: string = '';
+  amount: number = 0;
+  miles: number = 0;
+  onNameChange(value: string) {
+    this.name = value;
+  }
+
+  onDateChange(value: string) {
+    this.date = value;
+  }
+  onAmountChange(value: string) {
+    this.amount = parseFloat(value);
+  }
+  onMilesChange(value: string) {
+    this.miles = parseFloat(value);
+  }
+}
